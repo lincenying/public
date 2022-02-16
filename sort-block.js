984ab49515e09bb1e29bfe82683ec6f1d0f2d4dd
林岑影 <lincenying@qq.com>
2022-02-16 14:31:48 +0800

    commit on 2022-02-16 14:31:48

--- sort-block.js
+++ sort-block.js
@@ -5,7 +5,8 @@ const trim = require('locutus/php/strings/trim')
 const data = fs.readFileSync('./txt/AdBlock.txt', 'utf-8')
 let BlockTxt = data.split('! start')[1].split('! end')[0]
 BlockTxt = trim(BlockTxt)
-const arrBlockTxt = BlockTxt.split('\n')
+let arrBlockTxt = BlockTxt.split('\n')
+arrBlockTxt = [...new Set(arrBlockTxt)]
 const newBlockTxt = arrBlockTxt.sort().join('\n')
 
 const writeBlockTxt = `[Adblock Plus 2.0]
@@ -14,8 +15,8 @@ const writeBlockTxt = `[Adblock Plus 2.0]
 ! Title: 自定义过滤规则
 ! Last modified: ${date('Y-m-d H:i:s')}
 ! Expires: 1 days (update frequency)
-! Homepage: https://github.com/lincenying/common-tools
-! Licence: https://github.com/lincenying/common-tools
+! Homepage: https://github.com/lincenying/public
+! Licence: https://github.com/lincenying/public
 !
 !-----------------------General advert blocking filters-----------------------!
 ! start
