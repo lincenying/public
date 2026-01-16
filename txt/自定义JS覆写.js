18016cd7e5d6c0761c4e27a988f1664f6a2bc88e
LinCenYing <lincenying@qq.com>
2026-01-16 15:57:51 +0800

    chore: update

--- txt/自定义JS覆写.js
+++ txt/自定义JS覆写.js
@@ -83,7 +83,8 @@ function main(config) {
         'DOMAIN-SUFFIX,update.googleapis.com,🛑全球拦截',
         'DOMAIN-SUFFIX,tools.google.com,🛑全球拦截',
         'DOMAIN-SUFFIX,dl.google.com,🛑全球拦截',
-        'DOMAIN-SUFFIX,clients2.google.com,🛑全球拦截',
+        // google同步
+        // 'DOMAIN-SUFFIX,clients2.google.com,🛑全球拦截',
         ...tmpRules,
         'DOMAIN-SUFFIX,auth0.openmai.com,💬ChatGPT',
         'DOMAIN-SUFFIX,platform.openai.com,💬ChatGPT',
